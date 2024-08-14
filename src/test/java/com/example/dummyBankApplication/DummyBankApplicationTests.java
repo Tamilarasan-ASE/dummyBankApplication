4e7495f7b5d443b0a8b89157b0826f4be311b32f
Tamilarasan-ASE <tamilrk2002@gmail.com>
2024-08-14 16:39:47 +0530

    Initial commit

---
+++ src/test/java/com/example/dummyBankApplication/DummyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummyBankApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
